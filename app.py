622c5fbf9966b97c5e3b234e33089c87fa3e6c49
baqar mustafa <baqarmustafa@baqars-MacBook-Air.local>
2026-01-30 14:29:20 +0530

    Initialize Flask backend structure

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "SubTrackr initialized"
+    return "Subscription Tracker API Running"
 
 if __name__ == "__main__":
     app.run(debug=True)
